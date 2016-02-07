bdc1b246e2bf522e141c5ac46dbf603049212c06
Tim Garwood <timgarwood@gmail.com>
2016-02-07 10:26:26 -0500

    Initial REST endpoint stuff, gitignore

---
+++ dodgeball/DodgeballCrudEndpoint.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nancy;
+using Nancy.Hosting.Self;
+
+namespace dodgeball
+{
+    /// <summary>
+    /// This endpoint handles the standard CRUD operations for
+    /// dodgeball related domain objects
+    /// </summary>
+    class DodgeballCrudEndpoint : Nancy.NancyModule
+    {
+        public DodgeballCrudEndpoint() : base()
+        {
+            Post["/season/add"] = AddSeason;
+            Get["/season/add"] = AddSeason;
+        }
+
+        private dynamic AddSeason(dynamic d)
+        {
+            Response response = new Response();
+            return response.WithStatusCode(200);
+        }
+    }
+}
---
+++ dodgeball/DodgeballMain.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nancy;
+using Nancy.Hosting.Self;
+
+namespace dodgeball
+{
+    /// <summary>
+    /// This class is the main entry point for the dodgeball server
+    /// </summary>
+    class DodgeballMain
+    {
+        static void Main(string[] args)
+        {
+            using(NancyHost host = new NancyHost(new Uri[] {new Uri("http://127.0.0.1:3000")}))
+            {
+                host.Start();
+                Console.ReadLine();
+            }
+        }
+    }
+}
